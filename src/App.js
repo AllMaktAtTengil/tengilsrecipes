5063c0c81468d500e34b37df4e36b016fe27d86a
Richardsonn1 <marcus.richardson@hotmail.se>
2021-09-09 15:49:47 +0200

    recipe api added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,38 @@ import MainContent from "./component/MainContent.jsx";
 import Footer from "./component/Footer.jsx";
 
 export default class App extends Component {
-    render() {
-        return (
-            <>
-                <div className="App">
-                    <Header />
-                    <MainContent />
-                    <Footer />
-                </div>
-            </>
-        );
+  state = { recipes: undefined };
+
+  async componentDidMount() {
+    const recipes = await this.getRecipes();
+    this.setState({
+      recipes: recipes,
+    });
+    console.log(recipes);
+  }
+
+  getRecipes = async () => {
+    const apikey = process.env.REACT_APP_APIKEY
+    try {
+      const apiCall = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=pasta&number=25&apiKey=${apikey}`);
+      const data = await apiCall.json();
+      return data;
+    } catch (error) {
+      alert(error);
     }
+
+  }
+
+  render() {
+    return (
+      <>
+        <div className="App">
+          <Header />
+          <MainContent />
+          <Footer />
+
+        </div>
+      </>
+    );
+  }
 }
--- src/component/MainContent.jsx
+++ src/component/MainContent.jsx
@@ -3,15 +3,15 @@ import { Component } from "react";
 import "../App.css";
 
 class MainContent extends Component {
-    render() {
-        return (
-            <>
-                <div className="maincontent">
-                    <h1>Main Content</h1>
-                </div>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div className="maincontent">
+          <h1>Header</h1>
+        </div>
+      </>
+    );
+  }
 }
 
 export default MainContent;
