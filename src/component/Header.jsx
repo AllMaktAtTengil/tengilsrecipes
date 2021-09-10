0b10ee049dac110feef39b505ae7cddae2921a61
Richardsonn1 <marcus.richardson@hotmail.se>
2021-09-10 10:00:34 +0200

    Header & css added.

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 import { Component } from "react";
 import "../App.css";
+import tengil from "../Images/tengil1.png";
+import Searchbar from "../component/Searchbar";
 
 class Header extends Component {
-    render() {
-        return (
-            <>
-                <div className="header">
-                    <h1>AWESOME HEADER</h1>
-                </div>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div className="header">
+          <div className="top-logo">
+            <img src={tengil} alt="logo" />
+          </div>
+          <div className="menu-wrapper">
+            <nav className="menu-nav">
+              <ul>
+                <li>
+                  <a href="#home">Home</a>
+                </li>
+                <li>
+                  <a href="#recipes">Recipes</a>
+                </li>
+                <li>
+                  <a href="#guides">Guides</a>
+                </li>
+                <li>
+                  <a href="#subscribe">Subscribe</a>
+                </li>
+                <li>
+                  <Searchbar />
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Header;
---
+++ src/component/Searchbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Searchbar extends Component {
+  render() {
+    return (
+      <form className="search-box" action="#">
+        <input
+          className="search-field"
+          type="text"
+          placeholder="search recipe"
+        ></input>
+      </form>
+    );
+  }
+}
+export default Searchbar;
