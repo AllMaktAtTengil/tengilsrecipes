60f6e01c1d8bf86d40d28a3342d217c2d05767ba
Richardsonn1 <marcus.richardson@hotmail.se>
2021-09-13 12:15:55 +0200

    added html to maincontent

--- src/component/MainContent.jsx
+++ src/component/MainContent.jsx
@@ -3,17 +3,37 @@ import { Component } from "react";
 import "../App.css";
 
 class MainContent extends Component {
-    render() {
-        return (
-            <>
-                <div className="maincontent">
-                    <div className="container">
-                        <h1>Header</h1>
-                    </div>
+  state = { recipes: [] };
+
+  getRecipes() {
+    const recipes = this.props.data;
+    this.setState({ recipes: recipes });
+  }
+
+  componentDidMount() {
+    this.getRecipes();
+  }
+
+  render() {
+    const { recipes } = this.state;
+    return (
+      <>
+        <div className="maincontent">
+          <div className="container">
+            {recipes.recipes &&
+              recipes.recipes.map((recipes, index) => (
+                <div key={index}>
+                  <div className="team-player-box" id={`tp-box-${index}`}>
+                    <h1>{recipes.title}</h1>
+                    <img src={recipes.image} alt="example" />
+                  </div>
                 </div>
-            </>
-        );
-    }
+              ))}
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default MainContent;
