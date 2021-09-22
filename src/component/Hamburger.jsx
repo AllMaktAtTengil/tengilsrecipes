36e6a6aff67d92627b084444af8bbca9abca9f1b
Richardsonn1 <marcus.richardson@hotmail.se>
2021-09-22 14:09:37 +0200

    css + small update

--- src/component/Hamburger.jsx
+++ src/component/Hamburger.jsx
@@ -3,50 +3,52 @@ import { elastic as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 
 class Hamburger extends Component {
-    render() {
-        return (
-            <div id="mobile-wrapper">
-                <Menu pageWrapId={"page-wrap"} outerContainerId={"mobile-wrapper"} disableAutoFocus customBurgerIcon={<img className="burger-icon" src="https://icon-library.com/images/free-hamburger-menu-icon/free-hamburger-menu-icon-3.jpg" alt="" />} right>
-                    <main id="page-wrap" className="page-wrap">
-                        <div className="mobile-nav">
-                            <p>
-                                <a id="home" className="menu-item" href="/">
-                                    Home
-                                </a>
-                            </p>
-                        </div>
-                        <div className="mobile-nav">
-                            <p>
-                                <Link to={`/raiting/meta-score`}>
-                                    <p>Best raiting</p>
-                                </Link>
-                            </p>
-                        </div>
-                        <div className="mobile-nav">
-                            <p>
-                                <Link to={`/raiting/popularity`}>
-                                    <p>Popular</p>
-                                </Link>
-                            </p>
-                        </div>
-                        <div className="mobile-nav">
-                            <p>
-                                <Link to={`/dw`}>
-                                    <p>DW's Favorites</p>
-                                </Link>
-                            </p>
-                        </div>
-                        <div className="mobile-nav">
-                            <p>
-                                <a id="contact" className="menu-item" href="/contact">
-                                    Contact us
-                                </a>
-                            </p>
-                        </div>
-                    </main>
-                </Menu>
+  render() {
+    return (
+      <div id="mobile-wrapper">
+        <Menu
+          pageWrapId={"page-wrap"}
+          outerContainerId={"mobile-wrapper"}
+          disableAutoFocus
+          customBurgerIcon={
+            <img
+              className="burger-icon"
+              src="https://icon-library.com/images/free-hamburger-menu-icon/free-hamburger-menu-icon-3.jpg"
+              alt=""
+            />
+          }
+          right
+        >
+          <main id="page-wrap" className="page-wrap">
+            <div className="mobile-nav">
+              <a id="home" className="menu-item" href="/">
+                Home
+              </a>
             </div>
-        );
-    }
+            <div className="mobile-nav">
+              <Link to={`/raiting/meta-score`}>
+                <p>Best raiting</p>
+              </Link>
+            </div>
+            <div className="mobile-nav">
+              <Link to={`/raiting/popularity`}>
+                <p>Popular</p>
+              </Link>
+            </div>
+            <div className="mobile-nav">
+              <Link to={`/dw`}>
+                <p>DW's Favorites</p>
+              </Link>
+            </div>
+            <div className="mobile-nav">
+              <a id="contact" className="menu-item" href="/contact">
+                Contact us
+              </a>
+            </div>
+          </main>
+        </Menu>
+      </div>
+    );
+  }
 }
 export default Hamburger;
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,49 +5,48 @@ import Searchbar from "../component/Searchbar";
 import Dropdown from "./Dropdown";
 import Hamburger from "./Hamburger";
 import { Link } from "react-router-dom";
-import ContactUs from "./ContactUs";
 
 class Header extends Component {
-    render() {
-        return (
-            <>
-                <div className="header">
-                    <div className="top-logo">
-                        <a href="http://localhost:3000/">
-                            <h2>Tengil's</h2>
-                            <h1>Recipes</h1>
-                        </a>
-                    </div>
-                    <Hamburger />
-                    <div className="menu-wrapper">
-                        <nav className="menu-nav">
-                            <ul className="nav-items">
-                                <li>
-                                    <a href="http://localhost:3000/" target="_self">
-                                        Home
-                                    </a>
-                                </li>
-                                <li>
-                                    <Dropdown />
-                                </li>
-                                <li>
-                                    <Link to={`/contact`}>
-                                        <p>Contact</p>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <a href="#subscribe-div">Subscribe</a>
-                                </li>
-                                <li className="search-box">
-                                    <Searchbar multiParentFetch={this.props.parentFetch} />
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div className="header">
+          <div className="top-logo">
+            <a href="http://localhost:3000/">
+              <h2>Tengil's</h2>
+              <h1>Recipes</h1>
+            </a>
+          </div>
+          <Hamburger />
+          <div className="menu-wrapper">
+            <nav className="menu-nav">
+              <ul className="nav-items">
+                <li>
+                  <a href="http://localhost:3000/" target="_self">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <Dropdown />
+                </li>
+                <li>
+                  <Link to={`/contact`}>
+                    <p>Contact</p>
+                  </Link>
+                </li>
+                <li>
+                  <a href="#subscribe-div">Subscribe</a>
+                </li>
+                <li className="search-box">
+                  <Searchbar multiParentFetch={this.props.parentFetch} />
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Header;
